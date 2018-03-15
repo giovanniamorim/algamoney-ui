c8a673c1ae02f43a13a39cbbc4d3cfbc768acda3
Giovanni Amorim <giovanni.amorim.ti@gmail.com>
2018-03-15 19:05:57 -0300

    Adicionando o @Hostlistener a uma diretiva

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 import { Renderer3 } from '@angular/core/src/render3/renderer';
 
 @Directive({
@@ -9,10 +9,19 @@ export class CampoColoridoDirective {
   constructor(
     private elementRef: ElementRef;
     private renderer: Renderer2
-  ) {
+  ) {}
+
+  @HostListener('focus')
+  aoGanharFoco() {
     console.log(this.elementRef.nativeElement);
     this.renderer.setStyle(this.elementRef.nativeElement,
     'background-color', 'yellow');
+  }
+  @HostListener('blur')
+  aoPerderFoco() {
+    console.log(this.elementRef.nativeElement);
+    this.renderer.setStyle(this.elementRef.nativeElement,
+    'background-color', 'transparent');
   }
 
 }
